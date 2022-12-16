3ac3752ef769f1761ad30164ffc8fe5b6f3cad7d
Jesus Martinez-Garza <jes.martinez.garza@gmail.com>
2022-12-16 11:08:13 -0500

    CodeWars solution for Fri. Dec. 16; 7kyu

---
+++ findTheCapitals.js
@@ -0,0 +1,16 @@
+// Instructions
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+// Example
+// Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+
+var capitals = function (word) {
+	// Write your code here
+  let indexArr = []
+  for(let i = 0; i<word.length; i++){
+    if(word.charAt(i) === word.charAt(i).toUpperCase()){
+      indexArr.push(i)
+    }
+  }
+  return indexArr
+};
